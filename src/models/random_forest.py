86946c39faf56a8d6ec44ab93884fc0aba96cbe5
Ladislav Pavlicek <ladislav.pavlicek.2004@gmail.com>
2025-12-28 14:42:52 +0100

    update

--- src/models/random_forest.py
+++ src/models/random_forest.py
@@ -3,14 +3,39 @@ import pandas as pd
 import numpy as np
 
 from sklearn.ensemble import RandomForestClassifier
-from joblib import dump
+import pickle
 
-data = pd.read_csv("../../data/featured/data.csv", sep=';')
+from pathlib import Path
 
-Xtrain = data.drop('teamA_win', axis=1)
-ytrain = data.teamA_win
+random_seed = 42
 
-model = RandomForestClassifier(max_depth=3, n_estimators=30)
-model.fit(Xtrain, ytrain)
+BASE_DIR = Path(__file__).resolve().parents[2]
+DATA_PATH = BASE_DIR / "data" / "featured" 
 
-dump(model, '../../models/random_forest.joblib')
+train = pd.read_csv(DATA_PATH / "train.csv", sep=',')
+val = pd.read_csv(DATA_PATH / "val.csv", sep=',')
+
+Xtrain = train.drop(columns=['teamA_win'])
+ytrain = train['teamA_win']
+
+Xval = val.drop(columns=['teamA_win'])
+yval = val['teamA_win']
+
+Xtrain['date'] = pd.to_datetime(Xtrain['date'])
+Xval['date'] = pd.to_datetime(Xval['date'])
+
+min_date = Xtrain['date'].min()
+max_date = Xtrain['date'].max()
+sample_weight = (Xtrain['date'] - min_date) / (max_date - min_date)
+
+Xtrain = Xtrain.drop(columns=['date'])
+Xval = Xval.drop(columns=['date'])
+
+rf = RandomForestClassifier(n_estimators=18, max_depth=8, min_samples_leaf=1, min_samples_split=5, random_state=random_seed)
+rf.fit(Xtrain, ytrain, sample_weight=sample_weight)
+
+SCRIPT_DIR = Path(__file__).parent
+MODEL_PATH = SCRIPT_DIR / "random_forest.pkl"
+
+with open(MODEL_PATH, 'wb') as f:
+    pickle.dump(rf, f)
